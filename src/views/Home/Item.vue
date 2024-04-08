c1ee50705181cd3ea7ec916f9c2531f4a36c9c35
iigmir <roc120j@gmail.com>
2024-04-08 15:03:40 +0800

    Add book link

--- src/views/Home/Item.vue
+++ src/views/Home/Item.vue
@@ -4,7 +4,9 @@
             <h2>{{ book }}</h2>
         </section>
         <section class="ts-content">
-            {{ isbn }}
+            <a v-bind:href="get_book_link(isbn)" target="_blank">
+                {{ isbn }}
+            </a>, by {{ author }}
         </section>
     </article>
 </template>
@@ -17,6 +19,10 @@ defineProps([
     "book",
     "author"
 ]);
+
+const get_book_link = (isbn) => `https://www.google.com/search?tbm=bks&q=isbn:${isbn}`
+// const get_book_link = (isbn) => `https://openlibrary.org/search?isbn=${isbn}`
+// const get_book_link = (isbn) => `https://en.wikipedia.org/wiki/Special:BookSources/${isbn}`;
 </script>
 
 <style scoped>

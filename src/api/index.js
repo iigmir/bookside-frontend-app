0a4819ef8997764a728883b652d0f20415fa1641
iigmir <roc120j@gmail.com>
2024-04-08 15:58:17 +0800

    Refactor API

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 const API_HOST = () => import.meta.env.VITE_API_HOST;
 
-export function GetBookList() {
-    const api = API_HOST() + "/books/";
-    const ajax = fetch(api).then(r => r.json());
-    return ajax;
+export async function GetBookList() {
+    const api = `${API_HOST()}/books`;
+    const r = await fetch(api);
+    return await r.json();
 }
 
-export function GetBookItem(isbn = "") {
-    const api = API_HOST() + "/books/" + isbn;
-    const ajax = fetch(api).then(r => r.json());
-    return ajax;
+export async function GetBookItem(isbn = "") {
+    const api = `${API_HOST()}/books/${isbn}`;
+    const r = await fetch(api);
+    return await r.json();
 }
